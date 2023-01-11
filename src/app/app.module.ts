3236e4ac4944c0b26cc0692eb75a4fd4392837d1
EnilsonNeto <enilsonneto8@gmail.com>
2023-01-11 12:52:52 -0300

    Criando Formulario da Aplicação.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,28 @@
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component'
-
-import { MatSidenavModule }from '@angular/material/sidenav';
-import { MatCardModule }from '@angular/material/card';
-import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-import { BlueDirective } from './directives/blue.directive';
-import { MatButtonModule }from '@angular/material/button';
+
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule }from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule }from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule }from '@angular/material/button';
+
+import { BlueDirective } from './directives/blue.directive';
 
 @NgModule({
   declarations: [
@@ -41,7 +44,11 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatSidenavModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    HttpClientModule
 
   ],
   providers: [],
